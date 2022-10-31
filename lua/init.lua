2e83d1f79f22047e670efb2b1232044d68264e6d
Karbob <karbobc@gmail.com>
2022-10-31 15:23:21 +0800

    feat: undotree compatible with `Windows`

--- lua/init.lua
+++ lua/init.lua
@@ -693,8 +693,15 @@ end)
 
 -- undotree
 table.insert(config, function()
-  local undodir = "/tmp/undodir"
-  if vim.fn.has("persistent_undo") == 1 then
+  local undodir
+  local path = "undodir"
+  local has = vim.fn.has
+  if has("win32") == 1 or has("win64") == 1 then
+    undodir = string.format("%s\\%s", vim.env.TEMP, path)
+  else
+    undodir = string.format("/tmp/%s", path)
+  end
+  if has("persistent_undo") == 1 then
     vim.fn.mkdir(undodir, "p", 0700)
     vim.opt["undodir"] = undodir
     vim.opt["undofile"] = true
