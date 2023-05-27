962bf37f3bfa31984af4642267a4357fd2ea3441
Karbob <karbobc@gmail.com>
2023-05-27 21:39:18 +0800

    feat(plugins): migrate `nvim-treesitter` plugin

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,62 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = true,
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  ---@type TSconfig
+  opts = {
+    -- A list of parser names, or "all"
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "cmake",
+      "comment",
+      "dot",
+      "dockerfile",
+      "go",
+      "gomod",
+      "gosum",
+      "lua",
+      "luadoc",
+      "json",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "sql",
+      "toml",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = {},
+    highlight = {
+      enable = true,
+      -- list of language that will be disabled
+      -- NOTE: these are the names of the parsers and not the filetype.
+      disable = {},
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+    },
+    rainbow = {
+      enable = true,
+      disable = {},
+      -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      extended_mode = true,
+      max_file_lines = 10000,
+    },
+  },
+  ---@param opts TSConfig
+  config = function(_, opts)
+    local plugin = require("nvim-treesitter.configs")
+    plugin.setup(opts)
+  end,
+}
