7cae78f30f1792f1e7c291d8ecfe653aff4ebbfb
Karbob <karbobc@gmail.com>
2023-05-27 16:46:38 +0800

    feat(plugins): add `lazy.nvim` plugin

--- lua/init.lua
+++ lua/init.lua
@@ -729,6 +729,78 @@ _M.setup = function()
   for _, func in pairs(config) do
     func()
   end
+  -- install lazy.nvim plugins
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+  require("lazy").setup({
+
+    spec = {
+      { import = "plugins" },
+    },
+
+    defaults = {
+      -- should plugins be lazy-loaded?
+      lazy = false,
+      version = nil,
+    },
+
+    ---@type number limit the maximum amount of concurrent tasks
+    concurrency = nil,
+
+    install = {
+      -- install missing plugins on startup. This doesn't increase startup time.
+      missing = true,
+      -- try to load one of these colorschemes when starting an installation during startup
+      colorscheme = { "vscode" },
+    },
+
+    -- automatically check for plugin updates
+    checker = {
+      enabled = true,
+      ---@type number? set to 1 to check for updates very slowly
+      concurrency = nil,
+      -- get a notification when new updates are found
+      notify = false,
+      -- check for updates every hour
+      frequency = 3600,
+    },
+
+    performance = {
+      cache = {
+        enabled = true,
+      },
+      -- reset the package path to improve startup time
+      reset_packpath = true,
+      rtp = {
+        -- reset the runtime path to $VIMRUNTIME and your config directory
+        reset = true,
+        ---@type string[]
+        -- add any custom paths here that you want to includes in the rtp
+        paths = {},
+        ---@type string[] list any plugins you want to disable here
+        disabled_plugins = {
+          -- "gzip",
+          -- "matchit",
+          -- "matchparen",
+          -- "netrwPlugin",
+          -- "tarPlugin",
+          -- "tohtml",
+          -- "tutor",
+          -- "zipPlugin",
+        },
+      },
+    },
+  })
 end
 
 return _M
