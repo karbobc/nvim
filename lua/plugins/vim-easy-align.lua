df36574e39b243bafcabb40c1d038b1681d27473
Karbob <karbobc@gmail.com>
2023-05-27 20:11:28 +0800

    feat(plugins): migrate `vim-easy-align` plugin

---
+++ lua/plugins/vim-easy-align.lua
@@ -0,0 +1,8 @@
+return {
+  "junegunn/vim-easy-align",
+  lazy = true,
+  keys = {
+    { "<leader>ea", "<Plug>(EasyAlign)", desc = "Easy Align" },
+    { "<leader>ea", "<Plug>(EasyAlign)", mode = "x", desc = "Easy Align" },
+  },
+}
