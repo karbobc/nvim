b841703bd67ef6643b1769f2b0fb1d08169b8b97
Karbob <karbobc@gmail.com>
2023-05-31 00:24:46 +0800

    feat(plugins): add lsp support

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -5,5 +5,10 @@ return {
   config = function()
     local plugin = require("nvim-autopairs")
     plugin.setup()
+    local ok, cmp = pcall(require, "cmp")
+    if ok then
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end
   end,
 }
