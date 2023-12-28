26b0fe507d62fff86f6e66bb2ec31031d0717d89
Karbob <karbobc@gmail.com>
2023-12-28 16:17:12 +0800

    feat(plugins): add nvim-treesitter-context plugin

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,55 +1,67 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = true,
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  ---@type TSconfig
-  opts = {
-    -- A list of parser names, or "all"
-    ensure_installed = {
-      "bash",
-      "c",
-      "cpp",
-      "cmake",
-      "comment",
-      "dockerfile",
-      "go",
-      "gomod",
-      "gosum",
-      "lua",
-      "luadoc",
-      "json",
-      "make",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "regex",
-      "rust",
-      "sql",
-      "toml",
-      "vim",
-      "vimdoc",
-      "yaml",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = true,
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    ---@type TSconfig
+    opts = {
+      -- A list of parser names, or "all"
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "cmake",
+        "comment",
+        "dockerfile",
+        "go",
+        "gomod",
+        "gosum",
+        "lua",
+        "luadoc",
+        "json",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "rust",
+        "sql",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      -- List of parsers to ignore installing (for "all")
+      ignore_install = {},
+      highlight = {
+        enable = true,
+        -- list of language that will be disabled
+        -- NOTE: these are the names of the parsers and not the filetype.
+        disable = {},
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
     },
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-    -- List of parsers to ignore installing (for "all")
-    ignore_install = {},
-    highlight = {
-      enable = true,
-      -- list of language that will be disabled
-      -- NOTE: these are the names of the parsers and not the filetype.
-      disable = {},
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
-    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      local plugin = require("nvim-treesitter.configs")
+      plugin.setup(opts)
+    end,
   },
-  ---@param opts TSConfig
-  config = function(_, opts)
-    local plugin = require("nvim-treesitter.configs")
-    plugin.setup(opts)
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = true,
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {},
+    config = function(_, opts)
+      local plugin = require("treesitter-context")
+      plugin.setup(opts)
+    end
+  },
 }
