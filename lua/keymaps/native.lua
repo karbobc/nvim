bcdf3ee9f69f14702b720722c62cf3749602b287
Karbob <karbobc@gmail.com>
2025-04-25 16:09:45 +0800

    feat: migrate to native OSC 52

--- lua/keymaps/native.lua
+++ lua/keymaps/native.lua
@@ -7,7 +7,17 @@ local options = {
 local _M = {}
 
 -- normal mode map
-_M.n = {}
+_M.n = {
+  -- yank
+  {
+    "<leader>y",
+    function()
+      vim.cmd("let @+ = @0")
+      vim.notify("Copied to the system clipboard.")
+    end,
+    options
+  }
+}
 
 -- insert mode map
 _M.i = {}
--- lua/options/native.lua
+++ lua/options/native.lua
@@ -41,5 +41,16 @@ return {
   ["g"] = {
     mapleader      = " ",
     maplocalleader = " ",
+    clipboard = {
+      name = "OSC 52",
+      copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+      },
+      paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+      },
+    }
   },
 }
